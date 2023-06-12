9e8a712567c27efae108bf57596bdbfe56aad69e
BergitTeenu <berjitteenu@gmail.com>
2023-06-12 16:58:30 +0530

    merged with master

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
 const Koa = require('koa')
 const unitRouter = require('./routes/unitRoutes.js')
 const clientRouter = require('./routes/clientRoutes.js');
+const projectRoute = require('./routes/projectRoutes')
 
 const bodyParser = require('koa-bodyparser')
 
 const app = new Koa();
-const projectRoute = require('./routes/projectRoutes')
+
 
 app.use(bodyParser({ enableTypes: ['json', 'text'] }));
 
-app.use(projectRoute.routes()).use(projectRoute.allowedMethods());
-//app.use(bodyParser())
 
 app.use(unitRouter.routes()).use(unitRouter.allowedMethods())
 app.use(clientRouter.routes()).use(clientRouter.allowedMethods());
+app.use(projectRoute.routes()).use(projectRoute.allowedMethods());
 
 module.exports = app;
 
